5b6c9107f6f526ae2f8ba0540a441f9d4590eed2
Matthew Lopez <61769072+matt793@users.noreply.github.com>
2022-02-01 14:33:13 -0700

    Edited for a global user

--- PreText.py
+++ PreText.py
@@ -2,89 +2,99 @@
 
 statements = {'1': """
 Hello,
-I have picked up your ticket. I would like to set up a Teams call, and a TeamViewer session to solve your ticket issue. Are you available?
-Sincerely,
 
-Matthew Lopez 
-IT Technician - Tier 1 Help Desk
-970.397.8816 (O)
-970.397.8816 (M)
+ I have picked up your ticket. I would like to set up a Teams call, and a TeamViewer session to solve your ticket issue. Are you available?
+ 
+ Sincerely,
 
-mlopez6@henselphelps.com""", 
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""", 
 # 1 is the Ticket Starter.
     
     '2':"""
-I am escalating your ticket to Application Support. They are the primary group in charge of handling this platform, and can answer any questions in further detail.
-Sincerely,
+ I am escalating your ticket to Application Support. They are the primary group in charge of handling this platform, and can answer any questions in further detail.
+ 
+ Sincerely,
 
-Matthew Lopez 
-IT Technician - Tier 1 Help Desk
-970.397.8816 (O)
-970.397.8816 (M)
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
 
-mlopez6@henselphelps.com""",  
+ User@henselphelps.com""",  
 # 2 is the send to App Support.
 
     '3':"""
-Hello again, 
-Are you still needing help on the issue of this ticket? All tickets close after no response for 3 days. Let me know how we can best get a hold of you. Thanks.
-Sincerely,
+Hello again,
 
-Matthew Lopez 
-IT Technician - Tier 1 Help Desk
-970.397.8816 (O)
-970.397.8816 (M)
+ Are you still needing help on the issue of this ticket? All tickets close after no response for 3 days. Let me know how we can best get a hold of you. Thanks.
+ 
+ Sincerely,
 
-mlopez6@henselphelps.com""", 
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""", 
 # 3 is the 3rd day reminder.
 
     '4':"""
 Hello,
-There seems to be a wider spread problem with this issue. Your ticket will now be linked to the reported problem for a faster recovery. We have our top experts on working on the solution, and we hope to have this platform up shortly.Sincerely,
 
-Matthew Lopez 
-IT Technician - Tier 1 Help Desk
-970.397.8816 (O)
-970.397.8816 (M)
+ There seems to be a wider spread problem with this issue. Your ticket will now be linked to the reported problem for a faster recovery. We have our top experts on working on the solution, and we hope to have this platform up shortly.Sincerely,
 
-mlopez6@henselphelps.com""", 
+ Sincerely,
+
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""", 
 # 4 is the outage statement.
 
     '5':"""
 Hello,
-I will be escalating your ticket to your local I.T. district office, because this issue may need in-person, and a hands-on hardware solution. They should be getting a hold of you shortly.
-Sincerely,
 
-Matthew Lopez 
-IT Technician - Tier 1 Help Desk
-970.397.8816 (O)
-970.397.8816 (M)
+ I will be escalating your ticket to your local I.T. district office, because this issue may need in-person, and a hands-on hardware solution. They should be getting a hold of you shortly.
+ Sincerely,
 
-mlopez6@henselphelps.com""", 
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""", 
 # 5 is the send to local I.T.
 
     '6':"""
 I have reached out to the user on Teams.
-Sincerely,
 
-Matthew Lopez 
-IT Technician - Tier 1 Help Desk
-970.397.8816 (O)
-970.397.8816 (M)
+ Sincerely,
 
-mlopez6@henselphelps.com""",
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""",
 # 6 is the post Teams reply.
 
     '7':"""
 
-Sincerely,
+ Sincerely,
 
-Matthew Lopez 
-IT Technician - Tier 1 Help Desk
-970.397.8816 (O)
-970.397.8816 (M)
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
 
-mlopez6@henselphelps.com""",
+ User@henselphelps.com""",
 # 7 is just the signature.
 
 
@@ -96,11 +106,11 @@ mlopez6@henselphelps.com""",
 # Porgrams from HotKeys:
 
     '1p':"""
-runas /user:user@henselphelps.com cleanmgr.exe""", 
+runas /user:User@henselphelps.com cleanmgr.exe""", 
 # Admin DiskClean.
 
     '2p':"""
-runas /user:user@henselphelps.com powershell.exe""", 
+runas /user:User@henselphelps.com powershell.exe""", 
 # Admin PowerShell.
 
     '3p':"""
