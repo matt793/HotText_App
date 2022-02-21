b560cc74abf8ffc237eb871ed50b37c98e74219e
Matthew Lopez <61769072+matt793@users.noreply.github.com>
2022-02-21 15:53:25 -0700

    Added statements

--- PreText.py
+++ PreText.py
@@ -115,6 +115,84 @@ A Duo reactivation link was sent to your phone number. If you have any more issu
  User@henselphelps.com""",
 # 8 Duo reactivation link.
 
+    '9':"""
+The ticket was closed due to inactivity.
+
+ Sincerely,
+
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""",
+# 9 closed due to inactivity.
+
+    '10':"""
+Thanks for letting me know you are ready, I will be with you very shortly. I'm currently finishing up a client's ticket at the moment. I will jump on to your ticket after I finish up here! Thank you for your patience.
+ 
+ Sincerely,
+
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""",
+# 10 ticket pause-er.
+
+
+
+#########################################################
+# Separator ----------------/---------------- Separator #                                               
+#########################################################
+
+    # Responces made by Brad Blair
+
+    '1b':"""
+I have requested a re-activation for your duo account. You should be receiving a couple text messages with instructions for getting DUO activated on your new device. Please let me know if you have any questions.
+ 
+ Sincerely,
+
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""",
+# 1b Duo Reactivation
+
+    '2b':"""
+You have been provisioned access to use the Hensel Phelps Docusign subscription. In approx 30-60 minutes you will be able to follow the steps below to access your account.
+ 
+ 1. Visit this site-> https://account.docusign.com/#/username (copy and paste).
+ 2. Enter your HP email address.
+ 3. Click Continue
+ 4. Click "USE COMPANY LOGIN" button on the next page.
+ 
+ Sincerely,
+
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""",
+# 2b DocuSign Access granted.
+
+    '3b':"""
+We currently have a limited number of AutoCAD, Revit, and Navisworks licenses. If you receive an error message regarding the license when launching the program, this means that all available licenses are currently in use. Please try launching to program again in 10 minutes to see if a license has been made available for you to use. If not, you may need to reach out to another employee who may have the program open but is currently not using it so that they can close it to free up a license. If you are only viewing a model, we highly suggest using the Autodesk viewers that are installed on your computer. These programs are DWG TrueView (For AutoCAD/Civil 3D Models) and Navisworks Freedom Viewer (For Navisworks Models). Should you have questions or concerns please contact your district's VDC Manager for more information.
+
+ Sincerely,
+
+ First Last names 
+ IT Technician - Tier 1 Help Desk
+ 970.000.0000 (O)
+ 970.000.0000 (M)
+
+ User@henselphelps.com""",
+# 3b VDC/AutoDesk Licenses.
+              
 
 #########################################################
 # Separator ----------------/---------------- Separator #                                               
